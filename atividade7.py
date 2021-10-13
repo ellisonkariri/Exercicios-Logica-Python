c52679492e76c5aba4ad12ec7d35e998a2d7093d
ellisonkariri <ellison.alencar@gmail.com>
2021-10-13 11:24:44 -0300

    Mais um exercicio

---
+++ atividade7.py
@@ -0,0 +1,10 @@
+a = int(input("digite o primeiro numero"))
+b = int(input("digite o segundo numero"))
+c = int(input("digite o terceiro numero"))
+
+if a > b > c:
+    print(f'{a}' " é o numero maior")
+if b > c > a:
+    print(f'{b}' " é o numero maior ")
+if c > b > a:
+    print(f'{c}' " é o numero maior")
